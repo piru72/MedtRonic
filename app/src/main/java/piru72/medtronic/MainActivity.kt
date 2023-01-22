1b3d265841fb5130f65fed2b3d3f053ae5986f4b
piru72 <parvezdirom2000@gmail.com>
2023-01-23 02:24:57 +0600

    add (login) : auto login enabled

--- app/src/main/java/piru72/medtronic/MainActivity.kt
+++ app/src/main/java/piru72/medtronic/MainActivity.kt
@@ -3,6 +3,11 @@ package piru72.medtronic
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import piru72.medtronic.LoginSignup.SignUpActivity
 import piru72.medtronic.databinding.ActivityMainBinding
 
@@ -10,8 +15,10 @@ import piru72.medtronic.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:  ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
 
         binding = ActivityMainBinding .inflate(layoutInflater)
         val view = binding.root
@@ -19,6 +26,22 @@ class MainActivity : AppCompatActivity() {
         binding.btnGetStarted.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (auth.currentUser?.isEmailVerified == true)
+            updateUI(auth.currentUser)
+
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null) {
+            Log.i("LoginActivity", "Update UI Called")
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
